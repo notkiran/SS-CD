9b830e271707e02afa0cd791e47dd8e07fa49a75
Kiran G <kiran.g@ewit.edu>
2019-05-28 12:30:10 +0530

    Update prg4.c

--- prg4.c
+++ prg4.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 #include<string.h>
 int k=0,z=0,i=0,j=0,c=0;
 char a[16],ac[20],stk[15],act[10];
@@ -7,9 +6,11 @@ void check();
 void main()
 {
   puts("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id");
-  puts("enter input string ");
-  gets(a); c=strlen(a); strcpy(act,"SHIFT->");
-  puts("stack \t input \t action");
+  puts("Enter input string ");
+  gets(a);
+  c=strlen(a);
+  strcpy(act,"SHIFT->");
+  puts("\n stack \t input    \t action");
   for(k=0,i=0; j<c; k++,i++,j++)
   {
 if(a[j]=='i' && a[j+1]=='d')
@@ -31,7 +32,7 @@ else
 	check();
    }
  }
-getch();
+
 }
 void check()
 {
@@ -39,28 +40,35 @@ strcpy(ac,"REDUCE TO E");
 for(z=0; z<c; z++)
 if(stk[z]=='i' && stk[z+1]=='d')
 {
-	stk[z]='E'; stk[z+1]='\0';
+	stk[z]='E';
+	stk[z+1]='\0';
 	printf("\n$%s\t%s$\t%s",stk,a,ac);
 	j++;
 }
 for(z=0; z<c; z++)
 if(stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='E')
 {
-	stk[z]='E'; stk[z+1]='\0'; stk[z+2]='\0';
+	stk[z]='E';
+	stk[z+1]='\0';
+	stk[z+2]='\0';
 	printf("\n$%s\t%s$\t%s",stk,a,ac);
 	i=i-2;
 }
 for(z=0; z<c; z++)
 if(stk[z]=='E' && stk[z+1]=='*' && stk[z+2]=='E')
 {
-	St-k[z]='E'; stk[z+1]='\0'; stk[z+1]='\0';
+	stk[z]='E';
+	stk[z+1]='\0';
+	stk[z+1]='\0';
 	printf("\n$%s\t%s$\t%s",stk,a,ac);
 	i=i-2;
 }
 for(z=0; z<c; z++)
 if(stk[z]=='(' && stk[z+1]=='E' && stk[z+2]==')')
 {
-	stk[z]='E'; stk[z+1]='\0'; stk[z+1]='\0';
+	stk[z]='E';
+	stk[z+1]='\0';
+	stk[z+1]='\0';
 	printf("\n$%s\t%s$\t%s",stk,a,ac);
 	i=i-2;
 }
