c09db5b94bfebc64c259e38a3937fe114aa64570
Kiran G <kiran.g@ewit.edu>
2019-05-28 12:06:09 +0530

    Update prg9.c

--- prg9.c
+++ prg9.c
@@ -2,23 +2,23 @@
 #include<stdlib.h>
 void FIFO(char [ ],char [ ],int,int);
 void lru(char [ ],char [ ],int,int);
-void opt(char [ ],char [ ],int,int);
 int main()
 {
 int ch,YN=1,i,l,f;
 char F[10],s[25];
-printf("\n\n\t Enter the no of empty frames: ");
+printf("\n Enter the no of empty frames: ");
 scanf("%d",&f);
-printf("\n\n\t Enter the length of the string: ");
+printf("\n Enter the length of the string: ");
 scanf("%d",&l);
-printf("\n\n\t Enter the string: ");
+printf("\n Enter the string: ");
 scanf("%s",s);
 for(i=0;i<f;i++)
 F[i]=-1;
 do
-{printf("\n\n\t*********** MENU ***********");
-printf("\n\n\t1:FIFO\n\n\ t2:LRU \n\n\ t3:EXIT");
-printf("\n\n\t Enter your choice: ");
+{
+printf("\n*********** MENU ***********");
+printf("\n 1:FIFO\n 2:LRU\n 3:EXIT");
+printf("\n Enter your choice: ");
 scanf("%d",&ch);
 switch(ch)
 {
@@ -36,14 +36,16 @@ F[i]=-1;
 }
 lru(s,F,l,f);
 break;
-case 4:
+case 3:
 exit(0);
 }
-printf("\n\n\t Do u want to continue IF YES PRESS 1\n\n\t IF NO PRESS 0 : ");
+printf("\n Do u want to continue IF YES PRESS 1\n IF NO PRESS 0 :");
 scanf("%d",&YN);
-}while(YN==1);return(0);
 }
-
+while(YN==1);
+return(0);
+}
+//FIFO
 void FIFO(char s[],char F[],int l,int f)
 {
 int i,j=0,k,flag=0,cnt=0;
@@ -57,8 +59,9 @@ flag=1;
 }
 if(flag==0)
 {
-printf("\n\t%c\t",s[i]);
-F[j]=s[i];j++;
+printf("\n\t %c\t",s[i]);
+F[j]=s[i];
+j++;
 for(k=0;k<f;k++)
 {
 printf(" %c",F[k]);
@@ -69,18 +72,18 @@ cnt++;
 else
 {
 flag=0;
-printf("\n\t %c \t",s[i]);
+printf("\n\t %c\t",s[i]);
 for(k=0;k<f;k++)
 {
-printf("%c",F[k]);
+printf(" %c",F[k]);
 }
-printf("\t No page-fault");
+printf("\t No Page-fault");
 }
 if(j==f)
 j=0;
 }
 }
-
+//LRU
 void lru(char s[],char F[],int l,int f)
 {
 int i,j=0,k,m,flag=0,cnt=0,top=0;
@@ -89,43 +92,36 @@ for(i=0;i<l;i++)
 {
 for(k=0;k<f;k++)
 {
-if(F[k]==s[i])
-{
+if(F[k]==s[i]) {
 flag=1;
 break;
 }
 }
-printf("\n\t %c \t",s[i]);
+printf("\n\t %c\t",s[i]);
 if(j!=f && flag!=1)
 {
 F[top]=s[i];
 j++;
 if(j!=f)
 top++;
+
 }
 else
 {
-if(flag!=1)
-{
-for(k=0;k<top;k++)
-{
-F[k]=F[k+1];
-}
+if(flag!=1) {
+for(k=0;k<top;k++) {
+F[k]=F[k+1]; }
 F[top]=s[i];
 }
-if(flag==1)
-{
-for(m=k;m<top;m++)
-{
-F[m]=F[m+1];
-}
+if(flag==1) {
+for(m=k;m<top;m++) {
+F[m]=F[m+1]; }
 F[top]=s[i];
-}
 }
 }
 for(k=0;k<f;k++)
 {
-printf("%c",F[k]);
+printf(" %c",F[k]);
 }
 if(flag==0)
 {
@@ -133,6 +129,7 @@ printf("\t Page-fault %d",cnt);
 cnt++;
 }
 else
-printf("\t No Page-fault");
+printf("\t No page fault");
 flag=0;
+}
 }
